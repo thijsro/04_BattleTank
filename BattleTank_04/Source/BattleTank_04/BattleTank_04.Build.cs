cbd22ab6f3bb3e9e3cd2439d6cb638c4a30516fa
Thijs Roem <thijsroem@gmail.com>
2017-05-09 19:38:03 +0200

    VID102 creating project 09-05-17

---
+++ BattleTank_04/Source/BattleTank_04/BattleTank_04.Build.cs
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class BattleTank_04 : ModuleRules
+{
+	public BattleTank_04(TargetInfo Target)
+	{
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
