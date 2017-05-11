970f56bf1ee8d4d59bfa84ce6f42d86bbd133997
Thijs Roem <thijsroem@gmail.com>
2017-05-11 11:14:58 +0200

    VID122 AI sees playertank 11-05-17

--- BattleTank_04/Source/BattleTank_04/Public/TankAIController.h
+++ BattleTank_04/Source/BattleTank_04/Public/TankAIController.h
@@ -14,8 +14,10 @@ class BATTLETANK_04_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
-public:
+private:
 	ATank* GetControlledTank() const;
+
+	ATank* GetPlayerTank() const;
 
 	virtual void BeginPlay() override;
 	
